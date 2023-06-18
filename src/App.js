7d4f1d3c2909bf722bc552e0ca40d1e81cc9aebb
Pinak <pdtendulkar140203@gmail.com>
2023-06-18 15:45:46 +0530

    Commit_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `https://dummyjson.com/users`
+        );
+        setData(response.data);
+        setError(null);
+        console.log(data.users[0].firstName);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [loading]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className="table" border={1} >
+            <th>
+                <tr>
+                    <th>User id</th>
+                    <th>First Name</th>
+                    <th>Age</th>
+                    <th>BirthDate</th>
+                    <th>BloodGroup</th>
+                </tr>
+            </th>
+                {data.users.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.firstName}</td>
+                        <td>{user.lastName}</td>
+                        <td>{user.age}</td>
+                        <td>{user.birthDate}</td>
+                        <td>{user.bloodGroup}</td>
+                    </tr>
+                ))}
+        </table>
     </div>
   );
 }
